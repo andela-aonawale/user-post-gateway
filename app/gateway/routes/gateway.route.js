f7f93c4547f44308d0fdc155fa6d298920884a95
ahmed onawale <ahmed.onawale@andela.co>
2015-04-20 20:46:02 +0100

    add users, user/:id, posts, post/:id, newpost, signin routes

---
+++ app/gateway/routes/gateway.route.js
@@ -0,0 +1,18 @@
+var gateway = require("../controllers/gateway.controller");
+
+module.exports = function(router){
+	router.route("/newpost").post(gateway.createPost);
+	router.route("/posts").get(gateway.getAllPosts);
+	router.route("/post/:id").get(gateway.getOnePost);
+
+	router.route("/users").get(gateway.getAllUsers);
+	router.route("/user/:username")
+	.post(gateway.signOutUser)
+	.delete(gateway.deleteUser)
+	.put(gateway.updateUser)
+	.get(gateway.getOneUser);
+
+	router.route("/signin").post(gateway.signInUser);
+	//router.route("/signup").post(gateway.createUser);
+
+}
