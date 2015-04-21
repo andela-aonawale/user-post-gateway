f40a9e51337e862669a73b666a55ba3ec28f34cd
ahmed onawale <ahmed.onawale@andela.co>
2015-04-21 18:11:09 +0100

    add production and test configurations

--- config/config.js
+++ config/config.js
@@ -16,6 +16,38 @@ module.exports = {
 				signup: "http://localhost:8111/api/v1/signup"
 			},
 			secret: 'ammyreal'
+		},
+
+		test: {
+			url: {
+				allPosts: "http://localhost:8000/api/v1/posts",
+				onePost: "http://localhost:8000/api/v1/post/",
+				createPost: "http://localhost:8000/api/v1/newpost",
+				updatePostAuthor: "http://localhost:8000/api/v1/posts/author/",
+
+				decodeUser: "http://localhost:8111/api/v1/decode",
+				allUsers: "http://localhost:8111/api/v1/users",
+				oneUser: "http://localhost:8111/api/v1/user/",
+				signin: "http://localhost:8111/api/v1/signin",
+				signup: "http://localhost:8111/api/v1/signup"
+			},
+			secret: 'ammyreal'
+		},
+
+		production: {
+			url: {
+				allPosts: "https://secure-escarpment-1698.herokuapp.com/api/v1/posts",
+				onePost: "https://secure-escarpment-1698.herokuapp.com/api/v1/post/",
+				createPost: "https://secure-escarpment-1698.herokuapp.com/api/v1/newpost",
+				updatePostAuthor: "https://secure-escarpment-1698.herokuapp.com/api/v1/posts/author/",
+
+				decodeUser: "https://whispering-inlet-1798.herokuapp.com/api/v1/decode",
+				allUsers: "https://whispering-inlet-1798.herokuapp.com/api/v1/users",
+				oneUser: "https://whispering-inlet-1798.herokuapp.com/api/v1/user/",
+				signin: "https://whispering-inlet-1798.herokuapp.com/api/v1/signin",
+				signup: "https://whispering-inlet-1798.herokuapp.com/api/v1/signup"
+			},
+			secret: '@ammyreal'
 		}
 
 	},
